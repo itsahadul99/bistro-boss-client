ee1fc263a02240ddfeecf3fc3b493b83ccd7f95c
itsahadul99 <itsahadul999@gmail.com>
2024-05-15 23:30:18 +0600

    basic project set up done

---
+++ src/page/Home/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div>
+            <h1 className="font-inter text-5xl">This is home</h1>            
+            <h1 className="font-cinzel text-5xl">This is home</h1>            
+        </div>
+    );
+};
+
+export default Home;
