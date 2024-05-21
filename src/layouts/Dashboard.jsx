36b3247a5424e7b58b6000cde6f9e9ddea605a11
itsahadul99 <itsahadul999@gmail.com>
2024-05-21 17:24:55 +0600

    added my cart on dashboard

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -3,13 +3,13 @@ import Sidebar from "../page/Dashboard/Sidebar/Sidebar";
 
 const Dashboard = () => {
     return (
-        <div className="flex">
+        <div className="flex bg-[#F6F6F6]">
             {/* Side bar */}
             <div >
                 <Sidebar />
             </div>
             {/* Outlet */}
-            <div className="flex-1">
+            <div className="flex-1 px-20 my-10 max-w-6xl mx-auto">
                 <Outlet />
             </div>
         </div>
--- src/page/Dashboard/MyCart/MyCart.jsx
+++ src/page/Dashboard/MyCart/MyCart.jsx
@@ -1,8 +1,61 @@
+import SectionTitle from "../../../components/SectionTitle";
+import useCart from "../../../hooks/Cart/useCart";
+import { RiDeleteBin6Line } from "react-icons/ri";
+
 const MyCart = () => {
+    const [cart, refetch] = useCart()
+    const totalPrice = cart.reduce((total, items) => total + items.price, 0)
     return (
-        <div>
-            <h1>This is my cart</h1>
-        </div>
+        <dispatchEvent>
+            <SectionTitle heading={"WANNA ADD MORE?"} subHeading={"My cart"} />
+            <div className="overflow-x-auto p-8 shadow-sm mt-12 bg-white">
+                <div className="text-[#151515] font-semibold my-5 flex justify-evenly text-3xl">
+                    <h1>Total Order: {cart.length}</h1>
+                    <h1>total price: ${totalPrice}</h1>
+                    <button className="btn bg-[#D1A054] font-cinzel font-medium text-lg text-white">Pay</button>
+
+                </div>
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr className="font-inter uppercase bg-[#D1A054] text-white p-5">
+                            <th>
+                            </th>
+                            <th>ITEM IMAGE</th>
+                            <th>ITEM NAME</th>
+                            <th>PRICE</th>
+                            <th>ACTION</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            cart.map((item, idx) => <tr key={item?._id}>
+                                <th>
+                                    {idx + 1}
+                                </th>
+                                <td>
+                                    <div className="size-20 items-center flex justify-center">
+                                        <img src={item?.image} alt="Item img" />
+                                    </div>
+                                </td>
+                                <td>
+                                    {item?.name}
+                                </td>
+                                <td>
+                                    ${item?.price}
+                                </td>
+                                <th>
+                                    <button className="btn btn-ghost text-white bg-[#B91C1C]">
+                                        <RiDeleteBin6Line size={20} />
+                                    </button>
+                                </th>
+                            </tr>)
+                        }
+                    </tbody>
+
+                </table>
+            </div>
+        </dispatchEvent>
     );
 };
 
